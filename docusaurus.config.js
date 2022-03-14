3b73a8c95ade7d757616ff490c586226a3c4f655
seldomelee <1163435945@qq.com>
2022-03-14 22:07:04 +0800

    2022.3.14

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,6 +59,36 @@ module.exports = {
       ],
     },
 
+
+    colorMode: {
+      // "light" | "dark"
+      //defaultMode: "dark",
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+
+      // Dark/light switch icon options
+      switchConfig: {
+        // Icon for the switch while in dark mode
+        darkIcon: '🌙',
+        lightIcon: '🌞',
+
+        // CSS to apply to dark icon,
+        // React inline style object
+        // see https://reactjs.org/docs/dom-elements.html#style
+        darkIconStyle: {
+          marginLeft: "2px",
+        },
+
+        // Unicode icons such as '\u2600' will work
+        // Unicode with 5 chars require brackets: '\u{1F602}'
+        //lightIcon: '\u{1F602}',
+
+        lightIconStyle: {
+          marginLeft: "1px",
+        },
+      },
+    },
+
     // 页脚配置
     footer: {
       style: 'dark',                    // 页脚风格
