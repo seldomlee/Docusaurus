f0b3f09ca13e8eac3b8719b93d87fd3243e4e379
seldomelee <1163435945@qq.com>
2022-03-14 22:04:14 +0800

    2022.3.14

--- sidebars.js
+++ sidebars.js
@@ -2,18 +2,19 @@ module.exports = {
   docs: [
     {
       type: 'category',
-      label: 'test',
+      label: 'Start',
       items: [
         'template',
         '正则表达式',
+        '21年7月练习记录'
       ],
     },
-    {
-      Start:[
-        'template',
-        '正则表达式',
-      ]
-    },
+    // {
+    //   Start:[
+    //     'template',
+    //     '正则表达式',
+    //   ]
+    // },
   ]
 
 };
